fc251022b162e8122d2627be654b2ba1a6c4fd68
Andrei Korotkikh <korotkikh.andrey@gmail.com>
2022-05-04 23:20:45 +0400

    3rd module code review issues v2

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,5 +1,12 @@
 function namify(users) {
-  return users.map(getName);
+  //gets non-empty-name users first
+  let usersWithNonEmptyNames = [];
+  for (let i in users) {
+    if (users[i].name != null && users[i].name != undefined) {
+      usersWithNonEmptyNames.push(users[i]);
+    } 
+  }
+  return usersWithNonEmptyNames.map(getName);
 }
 
 function getName(user) {
