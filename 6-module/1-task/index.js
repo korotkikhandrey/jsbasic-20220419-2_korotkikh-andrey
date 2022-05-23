08ae58b747a779341ec3eff66241b5fe78198d03
Andrei Korotkikh <korotkikh.andrey@gmail.com>
2022-05-23 11:15:24 +0400

    6 module 1st task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,24 @@
  */
 export default class UserTable {
   constructor(rows) {
+
+    this.elem = document.createElement('TABLE');
+    
+    this.elem.innerHTML = `<thead><td>Имя</td><td>Возраст</td><td>Зарплата</td><td>Город</td></thead>` + 
+    rows.map(row => {
+      return `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button class="remove-button">[x]</button></td></tr>`;
+    }).join('');
+
+    let rmvBtns = this.elem.querySelectorAll('.remove-button');
+
+    for(let btn of rmvBtns) {
+      btn.addEventListener('click', (event) => {
+        let parentTr = event.target.closest('tr');
+        parentTr.parentNode.removeChild(parentTr);
+      });
+    }
+
   }
 }
+
+
